5f484895f339a3f21aa574901ae4d7bb8973545f
bug00126 <daniel860126@gmail.com>
2017-05-22 23:06:07 +0800

    第一次

---
+++ Assets/script/CheckPassword.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheckPassword : MonoBehaviour {
+	[SerializeField] Text field;
+	[SerializeField] int pass = 0;
+
+	public bool checkPassword () {
+		if (int.Parse(field.text) == pass)
+			return true;
+		return false;
+	}
+}
---
+++ Assets/script/PasswordSubmit.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PasswordSubmit : MonoBehaviour {
+	[SerializeField] Text[] field;
+	[SerializeField] int[] passwd = {0, 0, 0};
+	GameObject canvas, passwdWindow;
+	//Image button;
+
+	void Awake () {
+		canvas = GameObject.Find("Canvas_passForComputer");
+		passwdWindow = GameObject.Find("computer_pass_block");
+		//button = Image.Find("computer_button");
+	}
+
+	public bool submitPasswd () {
+		bool flag = true;
+		for (int i=0; i<3; i++) {
+			if (int.Parse(field[i].text) != passwd[i]) {
+				flag = false;
+			}
+		}
+
+		if (flag)
+			print ("unlock");
+		else
+			print ("wrong password!!!");
+
+		return flag;
+	}
+
+	public void react() {
+		if (submitPasswd()) {
+			canvas.SetActive(false);
+			passwdWindow.SetActive(false);
+			//utton.Image.sprite = "1173763";
+		} else {
+
+		}
+	}
+}
