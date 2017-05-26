67e503184371ac47ed71fa311d33eda37d120008
bug00126 <daniel860126@gmail.com>
2017-05-26 10:09:00 +0800

    folder connect and check done (2017.05.26 10:08)

---
+++ Assets/script/FolderToNext.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FolderToNext : MonoBehaviour {
+
+	public FolderCheck[] fc;
+	[SerializeField] GameObject obj1, obj2;
+	bool flag = true;
+	public bool checkNextFolder () {
+		for (int i=0; i<5; i++) {
+			if (!fc[i].flag) flag = false;
+		}
+		return flag;
+	}
+
+	public void react () {
+		if (checkNextFolder()) {
+			obj1.SetActive(true);
+			print("Correct");
+		} else {
+			obj2.SetActive(false);
+			print("Wrong");
+		}
+	}
+}
