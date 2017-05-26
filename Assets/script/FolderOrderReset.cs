d225b6924e48e4c766430f166ac3b5c44d5aa3d3
bug00126 <daniel860126@gmail.com>
2017-05-27 00:07:53 +0800

    新增資料夾破解成功畫面

---
+++ Assets/script/FolderOrderReset.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FolderOrderReset : MonoBehaviour {
+
+	[SerializeField] FolderCheck[] fc;
+	public void folderOrderReset() {
+		for (int i=0; i<5; i++) {
+			fc[i].flag = false;
+		}
+	}
+}
