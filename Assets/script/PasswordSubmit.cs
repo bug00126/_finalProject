159d1027d2e1a1ab0e2df6e50d4672eee2b6edea
vickychenchen <vickychen199700@gmail.com>
2017-06-01 16:55:32 +0800

    mailbox complete. secret mail password have to be completed.

--- Assets/script/PasswordSubmit.cs
+++ Assets/script/PasswordSubmit.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class PasswordSubmit : MonoBehaviour {
 	public Text[] field;
-	public int[] passwd;
+	public string[] passwd;
 	public int size;
 	public Sprite sprite;
 	[SerializeField] GameObject passwdWindow, button;
@@ -21,7 +21,7 @@ public class PasswordSubmit : MonoBehaviour {
 	public bool submitPasswd () {
 		bool flag = true;
 		for (int i=0; i<size; i++) {
-			if (int.Parse(field[i].text) != passwd[i]) {
+			if (field[i].text != passwd[i]) {
 				flag = false;
 			}
 		}
