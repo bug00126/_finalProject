b0267dce3dfd4d4733979f7d661d15156cc67731
bug00126 <daniel860126@gmail.com>
2017-06-14 23:16:46 +0800

    music change

--- Assets/script/Ending.cs
+++ Assets/script/Ending.cs
@@ -1,16 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class Ending : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	[SerializeField] Image[] image;
+	[SerializeField] Text[] text;
+	[SerializeField] CanvasGroup canvas;
+
+	void Awake() {
+		print ("awake");
+		this.myInvoke(1.52f, ()=> {
+			image[0].DOFade(1, 1);
+		});
+		this.myInvoke(3.02f, ()=> {
+			image[1].DOFade(1, 1);
+		});
+		this.myInvoke(4.52f, ()=> {
+			image[2].DOFade(1, 1);
+			text[0].DOFade(1, 1);
+			image[3].DOFade(1, 1);
+			text[1].DOFade(1, 1);
+			canvas.interactable = true;
+		});
 	}
 }
