563a261288aae2b6ebee713cc2b4d2730b86640d
bug00126 <daniel860126@gmail.com>
2017-06-15 02:06:58 +0800

    the ending scene complete

--- Assets/script/Ending.cs
+++ Assets/script/Ending.cs
@@ -6,24 +6,89 @@ using DG.Tweening;
 
 public class Ending : MonoBehaviour {
 
-	[SerializeField] Image[] image;
-	[SerializeField] Text[] text;
-	[SerializeField] CanvasGroup canvas;
+	[SerializeField] Fade[] fade;
+	[SerializeField] GameObject[] text;
+	[SerializeField] float[] second;
 
 	void Awake() {
-		print ("awake");
-		this.myInvoke(1.52f, ()=> {
-			image[0].DOFade(1, 1);
+		this.myInvoke(second[0], ()=> {
+			fade[0].fade();
 		});
-		this.myInvoke(3.02f, ()=> {
-			image[1].DOFade(1, 1);
+		this.myInvoke(second[1], ()=> {
+			fade[1].fade();
 		});
-		this.myInvoke(4.52f, ()=> {
-			image[2].DOFade(1, 1);
-			text[0].DOFade(1, 1);
-			image[3].DOFade(1, 1);
-			text[1].DOFade(1, 1);
-			canvas.interactable = true;
+		this.myInvoke(second[2], ()=> {
+			fade[2].fade();
+		});
+
+		this.myInvoke(second[3], ()=> {
+			text[0].SetActive(true);
+		});
+		this.myInvoke(second[4], ()=>{
+			text[0].SetActive(false);
+		});
+
+		this.myInvoke(second[5], ()=>{
+			text[1].SetActive(true);
+		});
+		this.myInvoke(second[6], ()=>{
+			text[1].SetActive(false);
+		});
+
+		this.myInvoke(second[7], ()=>{
+			text[2].SetActive(true);
+		});
+		this.myInvoke(second[8], ()=>{
+			text[2].SetActive(false);
+		});
+
+		this.myInvoke(second[9], ()=>{
+			text[3].SetActive(true);
+		});
+		this.myInvoke(second[10], ()=>{
+			text[3].SetActive(false);
+		});
+
+		this.myInvoke(second[11], ()=>{
+			text[4].SetActive(true);
+		});
+		this.myInvoke(second[12], ()=>{
+			text[4].SetActive(false);
+		});
+
+		this.myInvoke(second[13], ()=>{
+			text[5].SetActive(true);
+		});
+		this.myInvoke(second[14], ()=>{
+			text[5].SetActive(false);
+		});
+
+		this.myInvoke(second[15], ()=>{
+			text[6].SetActive(true);
+		});
+		this.myInvoke(second[16], ()=>{
+			text[6].SetActive(false);
+		});
+
+		this.myInvoke(second[17], ()=>{
+			text[7].SetActive(true);
+		});
+		this.myInvoke(second[18], ()=>{
+			text[7].SetActive(false);
+		});
+
+		this.myInvoke(second[19], ()=>{
+			text[8].SetActive(true);
+		});
+		this.myInvoke(second[20], ()=>{
+			text[8].SetActive(false);
+		});
+
+		this.myInvoke(second[21], ()=>{
+			text[9].SetActive(true);
+		});
+		this.myInvoke(second[22], ()=>{
+			text[9].GetComponent<Text>().DOFade(0, 1);
 		});
 	}
 }
