284881241c1f928e3e8751f9e46802b13123c10c
bug00126 <daniel860126@gmail.com>
2017-06-14 19:50:58 +0800

    final set

---
+++ Assets/script/ContinueYesOrNo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContinueYesOrNo : MonoBehaviour {
+
+	public List<GameObject> obj = new List<GameObject>();
+	public SceneChange sc;
+
+	public void react () {
+		for (int i=0; i<obj.Count; i++) {
+			obj[i].SetActive(false);
+		}
+	}
+
+	public void react2() {
+		for (int i=0; i<obj.Count; i++) {
+			obj[i].SetActive(false);
+		}
+		sc.sceneChange();
+	}
+}
---
+++ Assets/script/DoorOpen.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class DoorOpen : Fade {
+
+	[SerializeField] SceneChange sc;
+
+	public override void fade() {
+		image.DOFade(fadeRate, duration).OnComplete (()=>{
+			sc.sceneChange();
+		});
+
+	}
+
+}
